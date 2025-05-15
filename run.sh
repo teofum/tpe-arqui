0f7324c95e3ba111eb75575c434d44a529085375
lolo-pb <lpizzutobeltran@gmail.com>
2025-05-15 11:39:11 -0300

    Modified run.sh para que compile y corra

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 
+sudo docker start CONT
+sudo docker exec -it CONT make clean -C /root/Toolchain
+sudo docker exec -it CONT make clean -C /root/
+sudo docker exec -it CONT make -C /root/Toolchain
+sudo docker exec -it CONT make -C /root/
+
+sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
+
+#sudo docker stop CONT
+
