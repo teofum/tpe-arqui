de72694cc9a6561167dd496cfc4a2d7c07d96e3c
lolo-pb <lpizzutobeltran@gmail.com>
2025-05-17 20:31:18 -0300

    cleeanup para teo <3

--- Kernel/include/kbd.h
+++ Kernel/include/kbd.h
@@ -4,12 +4,6 @@
 
 extern uint8_t _kbd_read();
 
-#define BUFFER_SIZE 64
-struct buffer{
-char* data[BUFFER_SIZE];
-int write_pos;
-uint8_t isChar; //flag
-};
 
 struct buffer _kbd_readKeyCombo();
 
