671072076a3c3d61e7aa32621d951e01b8f6ef2a
lolo-pb <lpizzutobeltran@gmail.com>
2025-05-17 13:08:02 -0300

    minor changes for testing, moved the buffer struct definition to kbd.h

--- Kernel/include/kbd.h
+++ Kernel/include/kbd.h
@@ -4,4 +4,13 @@
 
 extern uint8_t _kbd_read();
 
+#define BUFFER_SIZE 64
+struct buffer{
+char* data[BUFFER_SIZE];
+int write_pos;
+uint8_t isChar; //flag
+};
+
+struct buffer _kbd_readKeyCombo();
+
 #endif
