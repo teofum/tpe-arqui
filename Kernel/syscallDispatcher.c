1552771ff50acbb3b7c6ca9588537b12ce6576fc
Tiziano Arcodaci <tarcodaci@itba.edu.ar>
2025-05-16 22:23:26 -0300

    minor change

--- Kernel/syscallDispatcher.c
+++ Kernel/syscallDispatcher.c
@@ -4,7 +4,7 @@
 
 #define MAX_SYSCALLS 16
 
-static syscall_t syscallTable[MAX_SYSCALLS] = { NULL };
+static syscall_t syscalls[MAX_SYSCALLS] = { NULL };
 
 void initSyscalls() {
   // registerSyscall(3, read);
@@ -14,11 +14,11 @@ void initSyscalls() {
 
 void registerSyscall(uint64_t syscallID, syscall_t function) {
   if (syscallID < MAX_SYSCALLS)
-    syscallTable[syscallID] = function;
+    syscalls[syscallID] = function;
 }
 
 uint64_t syscallDispatcher(uint64_t syscallID, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5, uint64_t arg6) {
-  if (syscallID < MAX_SYSCALLS && syscallTable[syscallID] != NULL)
-    return syscallTable[syscallID](arg1, arg2, arg3, arg4, arg5, arg6);
+  if (syscallID < MAX_SYSCALLS && syscalls[syscallID] != NULL)
+    return syscalls[syscallID](arg1, arg2, arg3, arg4, arg5, arg6);
 }
 
