742dc43cf46e7a7dc89be967a27f07180d8171df
Tiziano Arcodaci <tarcodaci@itba.edu.ar>
2025-05-16 22:10:29 -0300

    variable name changed

--- Kernel/syscallDispatcher.c
+++ Kernel/syscallDispatcher.c
@@ -12,13 +12,13 @@ void initSyscalls() {
   // etc.
 }
 
-void registerSyscall(uint64_t syscallNumber, syscall_t function) {
-  if (syscallNumber < MAX_SYSCALLS)
-    syscallTable[syscallNumber] = function;
+void registerSyscall(uint64_t syscallID, syscall_t function) {
+  if (syscallID < MAX_SYSCALLS)
+    syscallTable[syscallID] = function;
 }
 
-uint64_t syscallDispatcher(uint64_t syscallNumber, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5, uint64_t arg6) {
-  if (syscallNumber < MAX_SYSCALLS && syscallTable[syscallNumber] != NULL)
-    return syscallTable[syscallNumber](arg1, arg2, arg3, arg4, arg5, arg6);
+uint64_t syscallDispatcher(uint64_t syscallID, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5, uint64_t arg6) {
+  if (syscallID < MAX_SYSCALLS && syscallTable[syscallID] != NULL)
+    return syscallTable[syscallID](arg1, arg2, arg3, arg4, arg5, arg6);
 }
 
