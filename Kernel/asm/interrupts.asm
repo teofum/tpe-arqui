a383ff5a2f256d2bad3d048fc8e01b50948f5604
Tiziano Arcodaci <tarcodaci@itba.edu.ar>
2025-05-18 14:25:38 -0300

    fix registers backup

--- Kernel/asm/interrupts.asm
+++ Kernel/asm/interrupts.asm
@@ -1,27 +1,19 @@
 SECTION .text
 
 %macro pushall 0
-	push rax
-	push rcx
-	push rdx
-	push rsi
-	push rdi
-	push r8
-	push r9
-	push r10
-	push r11
+	push rbx
+	push r12
+	push r13
+	push r14
+	push r15
 %endmacro
 
 %macro popall 0
-	pop r11
-	pop r10
-	pop r9
-	pop r8
-	pop rdi
-	pop rsi
-	pop rdx
-	pop rcx
-	pop rax
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbx
 %endmacro
 
 
