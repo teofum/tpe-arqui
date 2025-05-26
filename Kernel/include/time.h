878a290ed30674b09d2e8acb624799228c515ecd
lolo-pb <lpizzutobeltran@gmail.com>
2025-05-26 14:42:23 -0300

    refino el timer.c

--- Kernel/include/time.h
+++ Kernel/include/time.h
@@ -1,8 +1,24 @@
 #ifndef _TIME_H_
 #define _TIME_H_
 
+#define TICKS_PER_SECOND 18
+
+typedef struct {
+  int hours;
+  int minutes;
+  int seconds;
+} Time;
+
 void timer_handler();
-int ticks_elapsed();
-int seconds_elapsed();
+
+unsigned int ticks_elapsed();
+
+unsigned int seconds_elapsed();
+
+unsigned int minutes_elapsed();
+
+unsigned int hours_elapsed();
+
+Time getTime(unsigned int ticks);
 
 #endif
--- Kernel/time.c
+++ Kernel/time.c
@@ -1,9 +1,29 @@
 #include <time.h>
 
+
 unsigned long ticks = 0;
 
 void timer_handler() { ticks++; }
 
-int ticks_elapsed() { return ticks; }
+unsigned int ticks_elapsed() { return ticks; }
 
-int seconds_elapsed() { return ticks / 18; }
+unsigned int seconds_elapsed() { return ticks / TICKS_PER_SECOND; }
+
+unsigned int minutes_elapsed() {
+  return seconds_elapsed() % 3600;
+}//(ticks/TICKS_PER_SECOND)%3600
+
+unsigned int hours_elapsed() {
+  return minutes_elapsed() / 60;
+}//((ticks/TICKS_PER_SECOND)%3600)/60
+
+Time getTime(unsigned int ticks) {
+  Time t;
+  unsigned int total_seconds = ticks / TICKS_PER_SECOND;
+
+  t.hours = total_seconds / 3600;
+  t.minutes = (total_seconds % 3600) / 60;
+  t.seconds = total_seconds % 60;
+
+  return t;
+}
