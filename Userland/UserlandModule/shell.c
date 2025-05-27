4e810c3f81e1523e743eb1d60a4f80623a29ec0a
Teo Fumagalli <teo.fum@gmail.com>
2025-05-27 14:01:16 -0300

    use printf in userland

--- Userland/UserlandModule/shell.c
+++ Userland/UserlandModule/shell.c
@@ -1,5 +1,6 @@
-#include "strings.h"
+#include <print.h>
 #include <shell.h>
+#include <strings.h>
 #include <syscall.h>
 
 #define CMDS_SIZE 64
@@ -17,15 +18,14 @@ typedef struct {
 command_t commands[CMDS_SIZE] = {0};
 
 static int echo(const char *args) {
-  _syscall(SYS_WRITES, args);
-  _syscall(SYS_PUTC, '\n');
+  printf("%s\n", args);
 
   return 0;
 }
 
 static int exit() { return RET_EXIT; }
 
-static void writePrompt() { _syscall(SYS_WRITES, "> "); }
+static void writePrompt() { printf("> "); }
 
 static void readCommand(char *buf) {
   int inputEnd = 0;
@@ -50,6 +50,8 @@ static int runCommand(const char *cmd) {
   char cmdName[256];
   cmd = strsplit(cmdName, cmd, ' ');
 
+  // Linear search all commands. Not super efficient, but the number of
+  // commands is quite small so we don't need to worry too much.
   int retcode = RET_UNKNOWN_CMD;
   for (int i = 0; i < CMDS_SIZE && commands[i].cmd; i++) {
     if (strcmp(cmdName, commands[i].cmd) == 0) {
@@ -58,9 +60,11 @@ static int runCommand(const char *cmd) {
     }
   }
   if (retcode == RET_UNKNOWN_CMD) {
-    _syscall(SYS_WRITES, "Unrecognized command '");
-    _syscall(SYS_WRITES, cmdName);
-    _syscall(SYS_WRITES, "'\nType 'help' for a list of available commands\n");
+    printf(
+      "Unrecognized command '%s'\n"
+      "Type 'help' for a list of available commands\n",
+      cmdName
+    );
   } else if (retcode == RET_EXIT) {
     return 1;
   }
@@ -72,6 +76,7 @@ static int runCommand(const char *cmd) {
 int startShell() {
   char cmdBuf[256];
 
+  // Register commands
   command_t cmd_exit = {
     .cmd = "exit",
     .entryPoint = exit,
@@ -83,6 +88,7 @@ int startShell() {
   };
   commands[1] = cmd_echo;
 
+  // Run the shell
   int exit = 0;
   while (!exit) {
     writePrompt();
@@ -90,7 +96,9 @@ int startShell() {
     exit = runCommand(cmdBuf);
   }
 
-  _syscall(SYS_WRITES, "Goodbye!\n");
+  // Return to kernel
+  // This should almost never happen!
+  printf("Goodbye!\n");
 
   return 0;
 }
