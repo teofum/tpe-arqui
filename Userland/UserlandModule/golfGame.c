ab7d0da3ee3a61587804defda5a21ffbdb92fd7d
lolo-pb <lpizzutobeltran@gmail.com>
2025-05-29 23:35:36 -0300

    inicio colitions, not working ... yet

--- Userland/UserlandModule/golfGame.c
+++ Userland/UserlandModule/golfGame.c
@@ -2,70 +2,133 @@
 #include <stdint.h>
 #include <vga.h>
 
+//la necesite la robe de vga, utils.h estaria bueno
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define sqr(x) ((x) * (x))
+#define sqroot(x) (x)//help
+//
+
+#define T 0.5
+
 typedef struct {
   float x;
   float y;
+
   float vx;
   float vy;
+
   float ax;
   float ay;
-} player_t;
 
-player_t mc = {VGA_WIDTH / 2, VGA_HEIGHT / 2, 0};
+  int size;//pensado para ser un radio, se puede usar para suplantar masa
 
-void drawPlayer() {
-  //   vga_pixel(mc.pos[0], mc.pos[1], 0xFFFF000F, 0);
-  vga_rect(mc.x, mc.y, mc.x + 10, mc.y + 10, 0xffFF0080, 0);
+  float
+    gama;// friction/arraste // NO es exacto, es una idea mas simple, ver si queremos meter masa
+} physicsObject_t;
+
+typedef struct {// TODO: hacerlo un bitfield, me olvide como hacerlo
+  uint8_t up;
+  uint8_t down;
+  uint8_t left;
+  uint8_t right;
+} direction_t;
+
+
+void drawObject(physicsObject_t obj) {
+  vga_rect(
+    (obj.x - obj.size), (obj.y - obj.size), (obj.x + (obj.size * 2)),
+    (obj.y + (obj.size * 2)), 0xffFF0080, 0
+  );
 }
 
 /*
 *   updates player info
 */
-void readImputs() {
-
-  float t =
-    0.5;//con esto se puede ajustar la velocidad pero ideal seria liquearlo con algun clock
-  float gama =
-    0.05;// con esto se ajusta el arrastre, mientras mas alto mas se frena, idealmente entre 1 y 0
-
+direction_t readImputs() {
   kbd_pollEvents();
-  uint8_t up = kbd_keydown(KEY_ARROW_UP);
-  uint8_t down = kbd_keydown(KEY_ARROW_DOWN);
-  uint8_t right = kbd_keydown(KEY_ARROW_RIGHT);
-  uint8_t left = kbd_keydown(KEY_ARROW_LEFT);
+  direction_t arrowKeys = {0};
+  arrowKeys.up = (kbd_keydown(KEY_ARROW_UP));
+  arrowKeys.down = kbd_keydown(KEY_ARROW_DOWN);
+  arrowKeys.right = kbd_keydown(KEY_ARROW_RIGHT);
+  arrowKeys.left = kbd_keydown(KEY_ARROW_LEFT);
 
+  return arrowKeys;
+}
+
+/*
+* Actualiza el estado sin aplicarle una aceleracion
+*/
+void updateObject(physicsObject_t *obj) {
+  direction_t dir = {0};
+  accelerateObject(&obj, &dir);
+}
+
+
+/*
+* Aplica aceleracion y actualiza el estado 
+*/
+void accelerateObject(physicsObject_t *obj, direction_t *dir) {
   //x
-  if (right || left) {
-    if (right) { mc.ax = ((mc.ax + 1) > 1) ? 1 : (mc.ax + 1); }
-    if (left) { mc.ax = ((mc.ax - 1) < -1) ? -1 : (mc.ax - 1); }
+  if (dir->right || dir->left) {
+    if (dir->right) { obj->ax = ((obj->ax + 1) > 1) ? 1 : (obj->ax + 1); }
+    if (dir->left) { obj->ax = ((obj->ax - 1) < -1) ? -1 : (obj->ax - 1); }
   } else {
-    mc.ax = -(mc.vx * gama);
+    obj->ax = -(obj->vx * obj->gama);
   }
 
   //y
-  if (up || down) {
-    if (up) { mc.ay = ((mc.ay - 1) < -1) ? -1 : (mc.ay - 1); }
-    if (down) { mc.ay = ((mc.ay + 1) > 1) ? 1 : (mc.ay + 1); }
+  if (dir->up || dir->down) {
+    if (dir->up) { obj->ay = ((obj->ay - 1) < -1) ? -1 : (obj->ay - 1); }
+    if (dir->down) { obj->ay = ((obj->ay + 1) > 1) ? 1 : (obj->ay + 1); }
   } else {
-    mc.ay = -(mc.vy * gama);
+    obj->ay = -(obj->vy * obj->gama);
   }
 
 
-  mc.vx += mc.ax * t;
-  mc.vy += mc.ay * t;
+  obj->vx += obj->ax * T;
+  obj->vy += obj->ay * T;
 
-  mc.x += mc.ax * t * t + mc.vx * t;
-  mc.y += mc.ay * t * t + mc.vy * t;
+  obj->x += obj->ax * T * T + obj->vx * T;
+  obj->y += obj->ay * T * T + obj->vy * T;
+}
+
+void doColition(physicsObject_t *a, physicsObject_t *b) {
+
+  if (checkColition(a, b)) {}
+}
+
+//asumiendo que son circulos
+int checkColition(physicsObject_t *a, physicsObject_t *b) {
+  float dist = sqroot(sqr(a->x - b->x) + sqr(a->y - b->y));
+  return (dist < (a->size + b->size));
 }
 
 /*
 * Setup y main game loop
 */
 int gg_startGame() {
+
+  physicsObject_t mc = {0};
+  mc.x = VGA_WIDTH / 2;
+  mc.y = VGA_HEIGHT / 2;
+  mc.gama = 0.1;
+  mc.size = 20;
+
+
+  physicsObject_t ball = {0};
+  ball.x = VGA_WIDTH / 4;
+  ball.y = VGA_HEIGHT / 2;
+  ball.gama = 0.1;
+  ball.size = 10;
+
   while (1) {
-    readImputs();
     vga_clear(0xFF00FF00);
-    drawPlayer();
+
+    direction_t input = readImputs();
+    accelerateObject(&mc, &input);
+
+    drawObject(mc);
+    drawObject(ball);
     vga_present();
   }
   return 0;
