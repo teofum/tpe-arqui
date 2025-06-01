464a7fe526dda0b865822964f7897b71c65883c9
lolo-pb <lpizzutobeltran@gmail.com>
2025-06-01 18:10:56 -0300

    more cleanuo

--- Userland/UserlandModule/golfGame.c
+++ Userland/UserlandModule/golfGame.c
@@ -1,50 +1,7 @@
-#include <kbd.h>
-#include <stdint.h>
-#include <vga.h>
-
+#include <golfGame.h>
 
 #define T 1
 #define VMAX 10
-
-//la necesite la robe de vga, utils.h estaria bueno
-#define abs(x) ((x) > 0 ? (x) : -(x))
-#define signo(x) ((x > 0) ? 1 : -1)
-#define sqr(x) ((x) * (x))
-#define chaeckMaxv(x) (x > VMAX ? VMAX : (x < -VMAX ? -VMAX : x))
-//
-
-
-typedef struct {
-  float x;
-  float y;
-
-  float vx;
-  float vy;
-
-  int size;//pensado para ser un radio, se puede usar para suplantar masa
-
-  float mass;// para colisions y podria afectar el gama
-  // si usamos la inversa de la masa nos ahoramos la division de float
-
-  float gama;// friction/arraste // NO es exacto, es una idea mas simple
-
-  color_t color;
-} physicsObject_t;
-
-typedef struct {
-  float x;
-  float y;
-
-  int size;
-  float incline;
-
-} enviroment_t;
-
-
-typedef struct {// por alguna extrana rason no puedo usar floats aca
-  int x;
-  int y;
-} vector_t;
 
 
 void drawObject(physicsObject_t *obj
