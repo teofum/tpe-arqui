c8b517ef386244576a13d43546fdcfc4b3ed8c7f
lolo-pb <lpizzutobeltran@gmail.com>
2025-06-01 18:02:45 -0300

    cleanup

--- Userland/UserlandModule/golfGame.c
+++ Userland/UserlandModule/golfGame.c
@@ -27,6 +27,8 @@ typedef struct {
   // si usamos la inversa de la masa nos ahoramos la division de float
 
   float gama;// friction/arraste // NO es exacto, es una idea mas simple
+
+  color_t color;
 } physicsObject_t;
 
 typedef struct {
@@ -46,13 +48,12 @@ typedef struct {// por alguna extrana rason no puedo usar floats aca
 
 
 void drawObject(physicsObject_t *obj
-) {// TODO, hace falta algo para dibujar un rectangulo
+) {// TODO, hace falta algo para dibujar un circulo
   vga_rect(
     (obj->x - obj->size), (obj->y - obj->size), (obj->x + obj->size),
-    (obj->y + obj->size), 0xffFF0080, 0
+    (obj->y + obj->size), obj->color, 0
   );
 }
-
 void drawHole(enviroment_t *env) {
   vga_rect(
     (env->x - env->size), (env->y - env->size), (env->x + env->size),
@@ -62,7 +63,8 @@ void drawHole(enviroment_t *env) {
 
 
 /*
-*   updates player info
+*   Reads player input and makes a vector of it 
+*   Note: onely 8 directions
 */
 vector_t readImputs() {
   kbd_pollEvents();
@@ -78,45 +80,17 @@ vector_t readImputs() {
   return arrowKeys;
 }
 
+
 /*
 * Actualiza el estado sin aplicarle una aceleracion
 */
 void updateObject(physicsObject_t *obj) {
-  float oldvx = obj->vx;// es para que no oscile dont worry about it
-  float oldvy = obj->vy;
-  float oldx = obj->x;//para bounds
-  float oldy = obj->y;
-
-  //add drag
-  obj->vx -= (obj->vx * obj->gama);
-  obj->vy -= (obj->vy * obj->gama);
-
-  //make sure that gamma doesnt push the other way
-  if ((oldvx * obj->vx) < 0) { obj->vx = 0; }//tiene que solo aplicar al gama
-  if ((oldvy * obj->vy) < 0) { obj->vy = 0; }
-
-  //check max vel
-  obj->vx = (chaeckMaxv(obj->vx));
-  obj->vy = (chaeckMaxv(obj->vy));
-
-  //update pos
-  obj->x += obj->vx * T;
-  obj->y += obj->vy * T;
-
-  //che maxBounds
-  if ((obj->x - obj->size) < 0 || (obj->x + obj->size) > VGA_WIDTH) {
-    obj->x = oldx;
-    obj->vx = -(obj->vx);
-  }
-  if ((obj->y - obj->size) < 0 || (obj->y + obj->size) > VGA_HEIGHT) {
-    obj->y = oldy;
-    obj->vy = -(obj->vy);
-  }
+  vector_t v0 = {0};
+  accelerateObject(obj, &v0);
 }
 
-
 /*
-* Aplica aceleracion y actualiza el estado 
+* Aplica "aceleracion" y actualiza el estado 
 */
 void accelerateObject(physicsObject_t *obj, vector_t *dir) {
   float oldvx = obj->vx;// es para que no oscile dont worry about it
@@ -124,11 +98,9 @@ void accelerateObject(physicsObject_t *obj, vector_t *dir) {
   float oldx = obj->x;//para bounds
   float oldy = obj->y;
 
-
   //add drag
   obj->vx -= (obj->vx * obj->gama);
   obj->vy -= (obj->vy * obj->gama);
-
   //make sure that drag doesnt push the other way
   if ((oldvx * obj->vx) < 0) { obj->vx = 0; }//tiene que solo aplicar al gama
   if ((oldvy * obj->vy) < 0) { obj->vy = 0; }
@@ -136,7 +108,6 @@ void accelerateObject(physicsObject_t *obj, vector_t *dir) {
   //add velocity
   obj->vx += (dir->x);// * obj->mass);
   obj->vy += (dir->y);// * obj->mass);
-
   //check max vel
   obj->vx = (chaeckMaxv(obj->vx));
   obj->vy = (chaeckMaxv(obj->vy));
@@ -144,7 +115,6 @@ void accelerateObject(physicsObject_t *obj, vector_t *dir) {
   //update pos
   obj->x += obj->vx * T;
   obj->y += obj->vy * T;
-
   //che maxBounds
   if ((obj->x - obj->size) < 0 || (obj->x + obj->size) > VGA_WIDTH) {
     obj->x = oldx;
@@ -156,9 +126,8 @@ void accelerateObject(physicsObject_t *obj, vector_t *dir) {
   }
 }
 
-
 /*
-* checks if a colition happens and applyes a repelinf accel
+* checks if a colition happens and applyes a repeling vel
 */
 void doCollision(physicsObject_t *a, physicsObject_t *b) {
 
@@ -176,7 +145,25 @@ void doCollision(physicsObject_t *a, physicsObject_t *b) {
     accelerateObject(a, &dir);
   }
 }
+/*
+* asumiendo que son circulos 
+* retorna el vetor de 'a' a 'b'
+*/ //notalolo: final menos inicial
+void checkCollision(physicsObject_t *a, physicsObject_t *b, vector_t *dir) {
 
+  float difx = b->x - a->x;
+  float dify = b->y - a->y;
+  float distsqr = sqr(difx) + sqr(dify);
+
+  if (distsqr <= sqr(b->size + a->size)) {
+    dir->x = difx;
+    dir->y = dify;
+  }
+}
+
+/*
+* checks if obj is in a hole or mount and applyes a apropiate vel
+*/
 void doEnviroment(enviroment_t *env, physicsObject_t *obj) {
   vector_t dir = {0};
   checkEnviroment(env, obj, &dir);
@@ -202,27 +189,12 @@ void checkEnviroment(enviroment_t *env, physicsObject_t *obj, vector_t *dir) {
 
 
 /*
-* asumiendo que son circulos 
-* retorna el vetor de 'a' a 'b'
-*/ //nota: final menos inicial
-void checkCollision(physicsObject_t *a, physicsObject_t *b, vector_t *dir) {
-
-  float difx = b->x - a->x;
-  float dify = b->y - a->y;
-  float distsqr = sqr(difx) + sqr(dify);
-
-  if (distsqr <= sqr(b->size + a->size)) {
-    dir->x = difx;
-    dir->y = dify;
-  }
-}
-
-/*
 * Setup y main game loop
 */
 int gg_startGame() {
 
   physicsObject_t mc = {0};
+  mc.color = 0xffFF0080;
   mc.x = VGA_WIDTH / 2;
   mc.y = VGA_HEIGHT / 2;
   mc.gama = 0.1;
@@ -231,6 +203,7 @@ int gg_startGame() {
 
 
   physicsObject_t ball = {0};
+  ball.color = 0xffFF00A0;
   ball.x = VGA_WIDTH / 4;
   ball.y = VGA_HEIGHT / 2;
   ball.gama = 0.05;
@@ -247,6 +220,7 @@ int gg_startGame() {
     updateObject(&ball);
     doCollision(&mc, &ball);
     doEnviroment(&hole, &mc);
+    doEnviroment(&hole, &ball);
     vector_t input = readImputs();
     accelerateObject(&mc, &input);
 
