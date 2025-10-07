3fbd1a8031c92920d44cf46209dddc170bed90c7
lolo-pb <lpizzutobeltran@gmail.com>
2025-10-07 23:45:06 +0000

    refactor con pqueues

--- Kernel/src/scheduler.c
+++ Kernel/src/scheduler.c
@@ -1,3 +1,4 @@
+#include <pqueue.h>
 #include <process.h>
 #include <scheduler.h>
 #include <types.h>
@@ -5,12 +6,7 @@
 #define next(x) x = (x + 1) % (MAX_PID + 1)
 
 typedef struct {
-  pid_t data[MAX_PID + 1];//todo max size
-  int write_pos, read_pos;
-} scheduler_queue_t;
-
-typedef struct {
-  scheduler_queue_t groups[MAX_PRIORITY + 1];
+  pqueue_t groups[MAX_PRIORITY + 1];
   priority_t next;
 } scheduler_priority_list_t;
 
@@ -27,36 +23,37 @@ static priority_t order[order_size] = {0, 0, 1, 0, 1, 2, 0, 1,
 static scheduler_priority_list_t spl = {0};
 int scheduler_force_next = 0;
 
+int scheduler_init() {
+  for (int i = 0; i <= MAX_PRIORITY; ++i) { spl.groups[i] = pqueue_create(); }
+  spl.next = 0;
+  return 0;
+}
+
 pid_t scheduler_next() {
-  scheduler_queue_t *scheduler_queue;
+  pqueue_t scheduler_queue;
   priority_t next = order[spl.next];
   spl.next = (spl.next + 1) % (order_size);
 
   for (int i = next; i <= MAX_PRIORITY; ++i) {
-    scheduler_queue = &spl.groups[i];
+    scheduler_queue = spl.groups[i];
 
-    if (scheduler_queue->write_pos != scheduler_queue->read_pos) {
+    if (!pqueue_empty(scheduler_queue)) {
 
-      pid_t next_pid = scheduler_queue->data[scheduler_queue->read_pos];
-      next(scheduler_queue->read_pos);
+      pid_t next_pid = pqueue_dequeue(scheduler_queue);
 
       proc_running_pid = next_pid;
       return next_pid;
     }
   }
 
-  //todas las queues vacias
   proc_running_pid = 0;
-  return 0;// idle process //todo esto noc si existe
+  return 0;
 }
+
 
 void scheduler_enqueue(pid_t pid) {
   proc_control_block_t *pcb = &proc_control_table[pid];
-  scheduler_queue_t *scheduler_queue = &spl.groups[pcb->priority];
+  pqueue_t scheduler_queue = spl.groups[pcb->priority];
 
-  scheduler_queue->data[scheduler_queue->write_pos] = pid;
-  next(scheduler_queue->write_pos);
-
-  if (scheduler_queue->write_pos == scheduler_queue->read_pos)
-    next(scheduler_queue->read_pos);// todo crash and die if queue full
+  pqueue_enqueue(scheduler_queue, pid);
 }
