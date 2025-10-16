bef6ade6a798034bf57e6ff5fe4be4680e88d6e6
Teo Fumagalli <teo.fum@gmail.com>
2025-10-16 18:24:32 -0300

    extract setfont process to file

--- Userland/src/shell.c
+++ Userland/src/shell.c
@@ -8,6 +8,7 @@
 #include <proc.h>
 #include <process.h>
 #include <scheduler.h>
+#include <setfont.h>
 #include <shell.h>
 #include <sound.h>
 #include <status.h>
@@ -51,63 +52,6 @@ static int echo(uint64_t argc, char *const *argv) {
 static int clear() {
   io_clear();
   return 0;
-}
-
-typedef struct {
-  const char *name;
-  vga_font_t id;
-} font_entry_t;
-
-font_entry_t fonts[] = {
-  {"default", VGA_FONT_DEFAULT},
-  {"tiny", VGA_FONT_TINY},
-  {"tiny bold", VGA_FONT_TINY_BOLD},
-  {"small", VGA_FONT_SMALL},
-  {"large", VGA_FONT_LARGE},
-  {"alt", VGA_FONT_ALT},
-  {"alt bold", VGA_FONT_ALT_BOLD},
-  {"future", VGA_FONT_FUTURE},
-  {"old", VGA_FONT_OLD},
-};
-size_t n_fonts = sizeof(fonts) / sizeof(font_entry_t);
-
-static int setfont(uint64_t argc, char *const *argv) {
-  if (argc < 2) {
-    printf(COL_RED "Missing font name\n" COL_RESET
-                   "Usage: setfont <font name>\n"
-                   "Hint: Type " COL_YELLOW "'setfont ls'" COL_RESET
-                   " for a list of fonts\n");
-    return 1;
-  }
-
-  if (strcmp(argv[1], "ls") == 0) {
-    for (int i = 0; i < n_fonts; i++) {
-      printf(COL_BLUE "%s\n", fonts[i].name);
-    }
-    return 0;
-  }
-
-  char name[CMD_BUF_LEN];
-  char *w = name;
-  for (size_t i = 1; i < argc; i++) {
-    w += sprintf(w, "%s", argv[i]);
-    if (i < argc - 1) w += sprintf(w, " ");
-  }
-
-  for (int i = 0; i < n_fonts; i++) {
-    if (strcmp(name, fonts[i].name) == 0) {
-      io_setfont(fonts[i].id);
-      return 0;
-    }
-  }
-
-  printf(
-    COL_RED "Unknown font name '%s'\n" COL_RESET "Hint: Type " COL_YELLOW
-            "'setfont ls'" COL_RESET " for a list of fonts\n",
-    name
-  );
-
-  return 2;
 }
 
 static int history() {
